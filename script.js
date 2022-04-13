4ea01bc88483780f23d69bc952f2082fa2372f24
Anto Contreras vP <101233367+AntoCvP@users.noreply.github.com>
2022-04-13 12:49:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+const menu = '<select onchange="location=this.value;"><option>Menu</option><option value="index.html">Portada</option><option value="ejemplo-1.html">Ejemplo STAR WARS</option><option value="ejemplo-2.html">Ejemplo SISMOS</option><option value="ejemplo-3.html">Ejemplo DIGIMON</option><option value="ejemplo-4.html">Ejemplo COVID</option><option value="ejemplo-5.html">Ejemplo TSUNAMI</option></select>';
+document.querySelector("body").innerHTML += menu;
